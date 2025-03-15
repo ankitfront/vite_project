0e501d0bfc048d86a33777c30bdf5b28d63c2513
ankitfront <ankitchakladar@gmail.com>
2025-03-15 10:28:12 +0530

    vite-React

---
+++ src/components/cards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function cards({username}) {
+  console.log(username);
+  return (
+     
+  );
+}
+
+export default cards
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import './index.css'
+// function MyApp(){
+//   return (
+//     <div>
+//       <h1>Custom App!</h1>
+//     </div>
+//   );
+// }
+// const element = (
+//   <a href="https://google.com"target='_blank'>visit to google</a>
+// )
+// const reactElement = React.createElement(
+//   'a',
+//   {href:"https://google.com",target:'_blank'},
+//   'click me to visit google'
+// )
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+   
+    <App/>
+  
+)
